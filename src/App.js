2d945d451331089de2e772f01a7f57e14f784c60
IsabelaSanchez <natyst_26@hotmail.com>
2020-09-11 06:22:08 -0500

    proyects

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import TechSkills from'./components/TechSkills';
 import SoftSkills from'./components/SoftSkills';
 import About from './components/About';
 import Contact from './components/Contact';
+import Projects from './components/Projects';
+
 function App() {
   return (  
     <Router>
@@ -17,6 +19,7 @@ function App() {
        <Route path='/tech-skills' exact component={TechSkills}/>
        <Route path='/sobre-mi' exact component={About}/>
        <Route path='/soft-skills' exact component={SoftSkills}/>
+       <Route path='/proyectos' exact component={Projects}/>
        <Route path='/contacto' exact component={Contact}/>
      </Switch>
     </Router>
